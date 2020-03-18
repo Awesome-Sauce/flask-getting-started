822c5d052038b46ba98243bac2db7a812bf5f2ab
Awesome-Sauce <58866764+Awesome-Sauce@users.noreply.github.com>
2020-03-17 20:52:14 -0700

    Setting up Web Deployment

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='flask-getting-started',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Charles Kypros',
+    author_email='',
+    description='Flashcard Python Web App'
+)
